1ff91cb1b8a376600e471f4ba7fdba3957f2d227
Jonas Nagstrup Terp <jonas@htv7.net>
2021-09-08 12:32:22 +0200

    Create b-9.c

---
+++ lecture_02/b-9.c
@@ -0,0 +1,1 @@
+// Write a program that takes the length and width of a rectangular yard and the length and width of a rectangular house in the yard. Your program should compute the time required to cut the grass at the rate of two square feet a second.
