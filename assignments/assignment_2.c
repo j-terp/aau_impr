932fba0cad93ba4a151d42e188be62d265e6e259
j-terp <jonas@htv7.net>
2021-09-13 22:23:14 +0200

    Rewrote after feedback

--- assignments/assignment_2.c
+++ assignments/assignment_2.c
@@ -7,19 +7,28 @@
 
 // Konstanter for konvertering
 # define SECONDS_PER_MINUTE 60
-# define SECONDS_PER_HOUR 3600
-# define SECONDS_PER_DAY 86400
-# define SECONDS_PER_WEEK 604800
+# define SECONDS_PER_HOUR (60 * SECONDS_PER_MINUTE)
+# define SECONDS_PER_DAY (24 * SECONDS_PER_HOUR)
+# define SECONDS_PER_WEEK (7 * SECONDS_PER_DAY)
 int main(void){
     int weeks, days, hours, minutes, seconds;
 
-    printf("Enter amount of seconds: ");scanf("%d", &seconds);
+    // Input af antal sekunder
+    printf("Enter amount of seconds: ")
+    ;scanf("%d", &seconds);
 
     // Konverterer antalet sekunder til formatet "uger, dage, timer, minuter og sekunder"
-    weeks = seconds / SECONDS_PER_WEEK;     seconds = seconds % SECONDS_PER_WEEK;
-    days = seconds / SECONDS_PER_DAY;       seconds = seconds % SECONDS_PER_DAY;
-    hours = seconds / SECONDS_PER_HOUR;     seconds = seconds % SECONDS_PER_HOUR;
-    minutes = seconds / SECONDS_PER_MINUTE; seconds = seconds % SECONDS_PER_MINUTE;
+    weeks = seconds / SECONDS_PER_WEEK;
+    seconds = seconds % SECONDS_PER_WEEK;
+    
+    days = seconds / SECONDS_PER_DAY;
+    seconds = seconds % SECONDS_PER_DAY;
+    
+    hours = seconds / SECONDS_PER_HOUR;
+    seconds = seconds % SECONDS_PER_HOUR;
+    
+    minutes = seconds / SECONDS_PER_MINUTE;
+    seconds = seconds % SECONDS_PER_MINUTE;
 
     printf("%d uger, %d dage, %d timer, %d minuter, %d sekunder.", weeks, days, hours, minutes, seconds);
     return 0;
