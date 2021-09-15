5399b7c2d1d8c245a2013a016ba54acb6498c594
j-terp <jonas@htv7.net>
2021-09-15 14:32:34 +0200

    Restructure and negatives

--- lecture_03/3-4.c
+++ lecture_03/3-4.c
@@ -20,6 +20,7 @@
 
 // Overvej ombyggelig brugen af if-else kæder kontra sekvenser af if kontra betingede udtryk med brug af den ternære operator ?:
 #include <stdio.h>
+#include <stdlib.h>
 
 // Konstanter for konvertering
 # define SECONDS_PER_MINUTE 60
@@ -31,48 +32,59 @@ int main(void) {
     scanf("%d", &seconds);
     printf("%d sekunder svarer til ", seconds);
 
-    // Konverterer antalet sekunder til formatet "timer, minuter og sekunder"
-    hours = seconds / SECONDS_PER_HOUR;
-    seconds = seconds % SECONDS_PER_HOUR;
-
-    minutes = seconds / SECONDS_PER_MINUTE;
-    seconds = seconds % SECONDS_PER_MINUTE;
-
-    // Printer ud timer hvis det er større en 0
-    if (hours > 0) {
-        printf("%d time", hours);
-        if (hours > 1) {
-            printf("r");
-        }
-        // Hvis de andre variable er større end 0, så printes modsvarende bindeord
-        if (minutes != 0 && seconds != 0) {
-            printf(", ");
-        }
-        else if ((minutes == 0 && seconds != 0) || (minutes != 0 && seconds == 0)) {
-            printf(" og ");
-        }
-    }
-    // Printer ud minuter hvis det er større en 0
-    if (minutes > 0) {
-        printf("%d minut", minutes);
-        if (minutes > 1) {
-            printf("ter");
-        }
-        // Hvis de andre variable er større end 0, så printes modsvarende bindeord
-        if (seconds != 0) {
-            printf(" og ");
-        }
-    }
-    // Printer ud sekunder hvis det er større en 0
-    if (seconds > 0) {
-        printf("%d sekund", seconds);
-        if (seconds > 1 || seconds == 0) {
-            printf("er");
-        }
+    // Kontrollerer negativt tal
+    if (seconds < 0) {
+        seconds = -seconds;
+        printf("negativ ");
     }
 
-    // Printer ud 0 hvis alle er 0
-    if (hours )
+    // Printer ud 0 hvis tiden er 0
+    if (seconds == 0) {
+        printf("%d sekunder", seconds);
+    }
+    else {
+        // Konverterer antalet sekunder til formatet "timer, minuter og sekunder"
+        hours = seconds / SECONDS_PER_HOUR;
+        seconds = seconds % SECONDS_PER_HOUR;
 
-    return 0;
+        minutes = seconds / SECONDS_PER_MINUTE;
+        seconds = seconds % SECONDS_PER_MINUTE;
+
+        // Printer ud timer hvis det er større end 0, plural om større end 1
+        if (hours > 0) {
+            printf("%d time", hours);
+            if (hours > 1) {
+                printf("r");
+            }
+            // Hvis de følgende variable ikke er 0, så printes modsvarende bindeord
+            if (minutes != 0 && seconds != 0) {
+                printf(", ");
+            }
+            else if ((minutes == 0 && seconds != 0) || (minutes != 0 && seconds == 0)) {
+                printf(" og ");
+            }
+        }
+
+        // Printer ud minuter hvis det er større end 0, plural om større end 1
+        if (minutes > 0) {
+            printf("%d minut", minutes);
+            if (minutes > 1) {
+                printf("ter");
+            }
+            // Hvis de følgende variable ikke er 0, så printes modsvarende bindeord
+            if (seconds != 0) {
+                printf(" og ");
+            }
+        }
+
+        // Printer ud sekunder hvis det er større end 0, plural om større end 1
+        if (seconds > 0) {
+            printf("%d sekund", seconds);
+            if (seconds > 1 || seconds == 0) {
+                printf("er");
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
