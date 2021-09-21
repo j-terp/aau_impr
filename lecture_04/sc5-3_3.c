500f52e7bf37a2a650dc5be25ed7c01aec4b7a86
j-terp <jonas@htv7.net>
2021-09-21 15:35:44 +0200

    Initial upload

---
+++ lecture_04/sc5-3_3.c
@@ -0,0 +1,15 @@
+// The following segment needs some revision. Insert braces where they are needed and correct the errors. The corrected code should take five integers and display their sum.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int count = 0, next_num, sum;
+    while (count <= 5)
+    count += 1;
+    print("Next number> ");
+    scanf("%d", &next_num);
+    next_num += sum;
+    printf("%d numbers were added; \n", count);
+    printf("their sum is %d.\n", sum);
+}
