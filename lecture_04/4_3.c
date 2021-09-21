7ed29c67eef4045c2e202edaed65a446d11ecde0
j-terp <jonas@htv7.net>
2021-09-21 16:42:49 +0200

    Solved 4.3

--- lecture_04/4_3.c
+++ lecture_04/4_3.c
@@ -3,3 +3,42 @@
 // Skriv først en version med for-løkker. Dernæst en version med while løkker.
 
 // Denne opgave stammer fra bogen C by Dissection - anvendt med tilladelse fra forlaget.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n, i, sum = 0;
+    printf("Integer> ");
+    scanf("%d", &n);
+    /* For-loops
+    if (n > 0) {
+        for (i = n; i <= 2*n; ++i) {
+            sum += i;
+        }
+    }
+    else if (n < 0) {
+        for (i = 2*n; i <= n; ++i) {
+            sum += i;
+        }
+    }
+    */
+    /* While-loops
+    if (n > 0) {
+        i = n;
+        while (i <= 2*n) {
+            sum += i;
+            i += 1;
+        }
+    }
+    else if (n < 0) {
+        i = 2*n;
+        while (i <= n) {
+            sum += i;
+            i += 1;
+        }
+    }
+    */
+    printf("Sum: %d", sum);
+
+}
