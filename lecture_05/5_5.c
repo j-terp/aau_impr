2bd040b0e287e015405237a9f3c7ad8855c0ad57
j-terp <jonas@htv7.net>
2021-10-13 21:32:32 +0200

    Solved 5.5

--- lecture_05/5_5.c
+++ lecture_05/5_5.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // Vi har tidligere i denne lektion mødt skudårsfunktionen
 
 int isLeapYear(int year){
@@ -20,6 +22,7 @@ int leapYear(int year) {
   return !(year % 4) && (year % 100) || !(year % 400);
 }
 
+/*
 int main(void) {
   int n;
   // Only takes positive integers
@@ -27,6 +30,21 @@ int main(void) {
   printf("Input positive integer> ");
   scanf("%d", &n);
   } while (n < 1);
-  printf("1: "), isLeapYear(n) ? print("true\n"): printf("false\n");
-  printf("2: "), leapYear(n) ? print("true\n"): printf("false\n");
+  printf("1: "), isLeapYear(n) ? printf("true\n"): printf("false\n");
+  printf("2: "), leapYear(n) ? printf("true\n"): printf("false\n");
 }
+*/
+int main(void) {
+  int n, exceptions = 0;
+  for (n = 1900; n <= 2100; n++) {
+    if (isLeapYear(n) != leapYear(n)) {
+      printf("For year %d, isLeapYear gives %d while leapYear gives %d", n, isLeapYear(n), leapYear(n));
+      exceptions = 1;
+    }
+  }
+  if (exceptions == 0) {
+    printf("There is no mismatch");
+  }
+}
+
+// Svar: Der er ingen undtagelser, begge funktioner gives samme resultat.
