ada8ae5592c1dabaf408952c911f40ce594665ec
j-terp <jonas@htv7.net>
2021-10-15 22:34:11 +0200

    Solved 6.6

--- lecture_06/6_6.c
+++ lecture_06/6_6.c
@@ -5,3 +5,27 @@
 // Brug funktionen til at udskrive en omregningstabel til omregning af 1, 2, 3, ... 100 dollars til de fire andre valutaer.
 
 // (Denne opgave svarer til programmeringsprojekt 1, side 384 i 7. udgave af lærebogen).
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void dollarConvert(double, double *, double *, double *, double *);
+
+int main(void) {
+  int limit = 100;
+  double dollar, euro, kroner, rubler, yen;
+
+  printf("Dollar |   Euro    |  Kroner   |  Rubler   |    Yen    \n");
+  printf("-------+-----------+-----------+-----------+-----------\n");
+  for (dollar = 1; dollar <= limit; dollar++) {
+    dollarConvert(dollar, &euro, &kroner, &rubler, &yen);
+    printf("%6.lf | %9.2f | %9.2f | %9.2f | %9.2f\n", dollar, euro, kroner, rubler, yen);
+  }
+}
+
+void dollarConvert(double usd, double *eur, double *dkk, double *rub, double *jpy) {
+  *eur = usd * 0.89;
+  *dkk = usd * 6.66;
+  *rub = usd * 66.43;
+  *jpy = usd * 119.9;
+}
