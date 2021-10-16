ef43e69f83f7878ff8c387e487899c08927fba11
j-terp <jonas@htv7.net>
2021-10-16 22:33:17 +0200

    small fix

--- lecture_06/6_4b.c
+++ lecture_06/6_4b.c
@@ -6,7 +6,7 @@ double multi_apply(double (*) (double), int, double), half(double);
 int main(void) {
   int n, s;
   double (*f) (double);
-  f = &half; 
+  f = half; 
 
   double res = multi_apply(f, 4, 16.0);
   printf("%f", res);
