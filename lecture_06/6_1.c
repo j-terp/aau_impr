11c8de83abb82961a4a3e1f1dbc0f241e50449e7
j-terp <jonas@htv7.net>
2021-10-18 14:33:49 +0200

    Solved 6.1

--- lecture_06/6_1.c
+++ lecture_06/6_1.c
@@ -3,3 +3,21 @@
 // Omskriv nu funktionen således at fahrenheit temperaturen returneres gennem en output parameter - en pointer. Omskriv også main, således at kaldet ændres til denne nye parameterprofil.
 
 // Hvilken version foretrækker du?
+
+#include <stdio.h>
+
+double celsiusToFahrenheit(double *c);
+
+int main(void){
+  double temp_1 = 0, temp_2 = 100;
+  celsiusToFahrenheit(&temp_1);
+  celsiusToFahrenheit(&temp_2);
+
+  printf("Freezing point: %6.2f F.\n", temp_1);
+  printf("Boiling point: %6.2f F.\n", temp_2);
+  return 0;
+}
+
+double celsiusToFahrenheit(double *c) {
+  *c = 9.0 / 5.0 * *c + 32.0;
+}
