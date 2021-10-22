b434e0fd4bcfba8b57324484d1cf6e97c18428d6
j-terp <jonas@htv7.net>
2021-10-22 14:28:13 +0200

    Converted future lectures files to instrcutions

--- lecture_09/10_4.c
+++
@@ -1,7 +0,0 @@
-// Skriv et program med en funktion der finder og returnerer det længste fælles endelse (suffix) af to ord.
-
-// Funktionen skal have tre parametre. De to ord (to tekststrenge) skal overføres som input parametre. Den længste fælles endelse skal returneres i en output parameter (også en tekststreng). (Hvordan markerer du forskellen på en input parameter og en output parameter, som begge er tekststrenge?) Overvej hvordan der bliver allokeret plads til tesktstrengen med den fælles endelse (vil du bruge statisk eller dynamisk allokering?).
-
-// Eksempelvis er "logi" den længste fælles endelse af ordene "datalogi" og "biologi". Og den længste fælles endelse af "program" og "diagram" er "gram".
-
-// Denne opgave svarer til opgave 8 side 533 i 6. udgave af lærebogen
