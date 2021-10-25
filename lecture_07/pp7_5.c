d8d8ebb66051a4dab93ad64da4d14796f796df53
j-terp <jonas@htv7.net>
2021-10-25 14:12:34 +0200

    Solved PP 7.5

--- lecture_07/pp7_5.c
+++ lecture_07/pp7_5.c
@@ -5,38 +5,37 @@ int check_digit_function(int *);
 
 int main(void) {
   int *digitArray;
-  int check_digit, arrayLength = 12, arrayNumber, odds, evens;
-  int barcode_1[] =
-      {0, 7, 9, 4, 0, 0, 8, 0, 4, 5, 0, 1},
-      barcode_2[] =
-      {0, 2, 4, 0, 0, 0, 1, 6, 2, 8, 6, 0},
-      barcode_3[] =
-      {0, 1, 1, 1, 1, 0, 8, 5, 6, 8, 0, 7},
-      barcode_4[] =
-      {0, 5, 1, 0, 0, 0, 1, 3, 8, 1, 0, 1};
-  int barcodes[] =
-  {*barcode_1, *barcode_2, *barcode_3, *barcode_4};
+  int arrayLength = 12, arrayNumber = 4,
+      odds, evens, check_digit, sum;
+  int barcodes[][12] = {
+    {0, 7, 9, 4, 0, 0, 8, 0, 4, 5, 0, 1},
+    {0, 2, 4, 0, 0, 0, 1, 6, 2, 8, 6, 0},
+    {0, 1, 1, 1, 1, 0, 8, 5, 6, 8, 0, 7},
+    {0, 5, 1, 0, 0, 0, 1, 3, 8, 1, 0, 1}
+  };
 
+  // Looping through every barcode
   for (int i = 0; i < arrayNumber; i++) {
     odds = 0;
     evens = 0;
-    /*int current_barcode[12] = barcode_3;
+    
+    // Looping throught the barcode digits
     for (int j = 0; j < (arrayLength - 1); j++) {
-      if (j % 2 == 1) {
-        odds += current_barcode[j];
+      // Adding the odd placed and even placed digits respectively
+      if (((j + 1) % 2) == 1) {
+        odds += barcodes[i][j];
       }
-      else if (j % 2 == 1 && j != (arrayLength - 1)){
-        evens += current_barcode[j];
+      else if (((j + 1) % 2) == 0 && j != (arrayLength - 1)) {
+        evens += barcodes[i][j];
       }
-      
     }
-    if (3 * odds + evens == (current_barcode[arrayLength - 1])) {
-      printf("validated.");
-    }
-    else {
-      printf("error in barcode");
-    }
-    */
+
+    // Calculates the check digit and compares it to the given check digit
+    sum = (3 * odds + evens) % 10;
+    check_digit = (sum == 0) ? 0 : 10 - sum;
+    if (check_digit == (barcodes[i][arrayLength - 1]))
+      printf("validated.\n");
+    else
+      printf("error in barcode\n");
   }
-  
 }
